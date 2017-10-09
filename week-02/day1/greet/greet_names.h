0372e7492c29252ff19f892f5b38964ff830d766
batorsimon <bator.simon8@gmail.com>
2017-10-09 16:14:36 +0200

    task greet done

---
+++ week-02/day1/greet/greet_names.h
@@ -0,0 +1,8 @@
+char a[] = "Greenfox welcomes:";
+
+//names
+char b[] = "Balazs";
+char c[] = "Gergo";
+char d[] = "Adam";
+char e[] = "Annamari";
+
--- week-02/day1/greet/main.c
+++ week-02/day1/greet/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// define a variable and assign the value `Greenfox` to it in a .h file
+// create a function called `greet` that greets it's input parameter
+// define more names to greet, call the function multiple times
+
+#include "greet_names.h"
+int greet();
+
 int main()
 {
-    printf("Hello world!\n");
+    greet();
     return 0;
+}
+
+int greet(greet_names)
+{
+    printf("%s\n%s\n%s\n%s\n%s\n", a,b,c,d,e);
 }
