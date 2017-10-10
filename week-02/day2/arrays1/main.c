86f4b9049a6d9972f1ad40432e9daed3f46d3680
batorsimon <bator.simon8@gmail.com>
2017-10-10 10:41:54 +0200

    arrays1 task done

---
+++ week-02/day2/arrays1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char my_name_array[] = "Simon Bator";
+    int i = 0;
+
+
+    for(i=0; i < sizeof my_name_array; i++){
+        printf("%c", my_name_array[i]);
+    }
+
+	//TODO:
+    // Print out the array character-by-character
+    return 0;
+}
