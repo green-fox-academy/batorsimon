05cc87bc92d2e2dae92f5930f455d0ca0dc54a54
batorsimon <bator.simon8@gmail.com>
2017-10-13 13:00:35 +0200

    armstrong done and fixed

--- week-02/day4/armstrong/main.c
+++ week-02/day4/armstrong/main.c
@@ -1,41 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    //int a[20];
+    //int value_number = 0;
     int number = 0;
     int i = 0;
     int remain = 0;
-    int power_remain = 0;
-    int a = 0;
-    int value_number = 0;
+    int sum_power = 0;
+    int sum = 0;
+    int length_number = 0;
 
     printf("Write a number:");
     scanf("%d", &number);
+    i = number;
 
+    while(i !=0){
+         i = i / 10;
+         length_number++;
+    }
     i = number;
 
     while(i != 0){
-        remain = i % 10;
-        value_number = value_number + remain;
+        remain = i % 10;  //digitek helyiértéke
         i = i / 10;
-        printf("i:%d\n", i);
-        printf("remain:%d\n", remain); // ezt kell majd hatványozni
-        printf("value_number:%d\n", value_number);
-
-        power_remain = (remain * remain * remain * remain);
-        a ++;
-        printf("a:%d\n", a);
-        printf("power remain:%d\n", power_remain);
-        if(power_remain * a){
-             printf("\nThe %d is an Armstrong number.\n", number);
-        }
-
+        // value_number = value_number + remain;  // 1+6+3+4= 14    számjegyek összege
+        // printf("value_number:%d\n", value_number);
+        sum_power = (int)pow(remain, length_number);  //az adott digitet a szám hosszuságára emelem
+        sum = sum_power + sum;                            //a hátványozás eredményét folyamatosan összeadom
+        printf("The first digit powered: %d", sum_power);
+        printf(" and added together: %d\n", sum); //ez lesz a hatványozás végeredménye
     }
 
-     printf("\nThe %d is an Armstrong number.\n", number);
-
+    if(number == sum){
+             printf("\nThe %d is an Armstrong number.\n", number);
+        } else {
+            printf("The %d is not an Armstrong number.\n", number);
+        }
     return 0;
 }
+
 
