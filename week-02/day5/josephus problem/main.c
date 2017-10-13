cc7b9d559612f5b5ab7fc740c4fd7c23a86f5b5c
batorsimon <bator.simon8@gmail.com>
2017-10-13 13:06:39 +0200

    started josephus problem

---
+++ week-02/day5/josephus problem/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Josephus Problem is when you have a circle of n people standing
+//you want to cross out every second person step-by-step until only one remains
+
+int main()
+{
+
+    return 0;
+}
