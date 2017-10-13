3d63955d2a530fc119b6911883d2ba9b237c192b
batorsimon <bator.simon8@gmail.com>
2017-10-13 14:47:34 +0200

    anagram finished and fixed

--- week-02/day4/anagram/main.c
+++ week-02/day4/anagram/main.c
@@ -4,18 +4,13 @@
 //Create a function named is anagram following your current language's style guide.
 //It should take two strings and return a boolean value depending on whether its an anagram or not.
 
-void anagram();
+int anagram(char[], char[]);
 
 int main()
 {
-    anagram();
-    return 0;
-}
-
-void anagram()
-{
     char input[20] = {0};
     char input2[20] = {0};
+    int check= 0;
 
     printf("Type in a word: ");
     scanf("%s", &input);
@@ -23,48 +18,43 @@ void anagram()
     printf("\nType an anagram of the given word: ");
     scanf("%s", &input2);
 
+    check = anagram(input, input2);
+
+    if (check == 1)
+        printf("\"%s\" and \"%s\" are anagrams.\n", input, input2);
+    else
+        printf("\"%s\" and \"%s\" are not anagrams.\n", input, input2);
+    return 0;
+}
+
+int anagram(char input[], char input2[])
+{
+    int arr1[26] = {0};
+    int arr2[26] = {0};
     int i = 0;
-    int j = 0;
-    int input_length = sizeof input / sizeof input[0];
-    int input2_length = sizeof input2 / sizeof input2[0];
 
-    if(input_length < input2_length) {
-            printf("You typed more letters than you should!");
-            exit(0);
-            } else if(input_length > input2_length ) {
-            printf("You typed less letters than you should!");
-            exit(0);
-            }
+   while (input[i] != '\0')
+   {
+      arr1[input[i]-'a']++;
+      i++;
+   }
 
-    for(i = 0; i < input_length; i++){
-            printf("%c\n", input[i]);
+   i = 0;
 
-    }
+   while (input2[i] != '\0')
+   {
+      arr2[input2[i]-'a']++;
+      i++;
+   }
+
+   for (i = 0; i < 26; i++)
+   {
+      if (arr1[i] != arr2[i])
+         return 0;
+   }
+
+   return 1;
 
 
-
-/*
-    for(i = 0; i < input_length; i++)
-    {
-        for(j = 0; j < input2_length; j++) {
-            if(input[i] == input2[j]){
-
-                    printf("True\n");
-
-            } else{
-            printf("False!\n");
-            }
-
-            if(input_length < input2_length) {
-            printf("You typed more letters than you should!");
-            exit(0);
-            } else if(input_length > input2_length ) {
-            printf("You typed less letters than you should!");
-            exit(0);
-            }
-
-        }
-
-    }  */
 
 }
