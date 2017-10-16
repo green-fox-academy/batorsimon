5cab28d13c1c1d3ac6cb06d0616f678a31aced45
batorsimon <bator.simon8@gmail.com>
2017-10-16 18:26:51 +0200

    to binary one solution...maybe

---
+++ week-03/day1/palindrome/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+void  palindrome(char input);
+
+int main()
+{
+	char input[100];
+	printf("please enter a word: \n");
+	scanf("%s", input);
+
+	palindrome(input[100]);
+
+	return 0;
+}
+
+void  palindrome(char input)
+{
+
+	char pointer;
+	int i = 0;
+
+	for (i; i < strlen(input); i++) {
+		pointer = input[i];
+			printf("%s", input[i]);
+
+
+		printf("%c", pointer);
+	}
+}
