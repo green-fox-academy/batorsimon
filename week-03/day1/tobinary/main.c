da7ed09ad6ed570faf65ead7ff76cf3bbe948cf3
batorsimon <bator.simon8@gmail.com>
2017-10-16 22:46:19 +0200

    tobinary finished but need to be checked by a mentor

--- week-03/day1/tobinary/main.c
+++ week-03/day1/tobinary/main.c
@@ -2,60 +2,23 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+/*  16     0001 0000
+    32     0010 0000
+
+   16 & 32 0000 0000           */
+
 int int_to_bin_digit(int in, int count)
 {
-    /*
+
 	int mask = 1U << (count);
 	int i = 0;
+    printf("The number %d and %d bitwise ANDed together is: ", in, count);
 	for (i = 0; i < count; i++) {
 		int result = (in & mask) ? 1 : 0;
 		printf("%d", result);
-		in = in < 1;
+		//in = in < 1;
 	}
-
-
-	16     0001 0000
-    32     0010 0000
-
-    16 & 32
-    0000 0000
-
-	*/
-
-	uint8_t a = 0b00010000;
-    printf("Binaris kiiratas a: ");
-    printf("%c", (a & 1<<7 ? '1' : '0'));
-    printf("%c", (a & 1<<6 ? '1' : '0'));
-    printf("%c", (a & 1<<5 ? '1' : '0'));
-    printf("%c", (a & 1<<4 ? '1' : '0'));
-    printf("%c", (a & 1<<3 ? '1' : '0'));
-    printf("%c", (a & 1<<2 ? '1' : '0'));
-    printf("%c", (a & 1<<1 ? '1' : '0'));
-    printf("%c", (a & 1    ? '1' : '0'));
-    printf("\n");
-
-    uint8_t b = 0b00100000;
-    printf("Binaris kiiratas b: ");
-    printf("%c", (a & 1<<7 ? '1' : '0'));
-    printf("%c", (a & 1<<6 ? '1' : '0'));
-    printf("%c", (a & 1<<5 ? '1' : '0'));
-    printf("%c", (a & 1<<4 ? '1' : '0'));
-    printf("%c", (a & 1<<3 ? '1' : '0'));
-    printf("%c", (a & 1<<2 ? '1' : '0'));
-    printf("%c", (a & 1<<1 ? '1' : '0'));
-    printf("%c", (a & 1    ? '1' : '0'));
-    printf("\n");
-
-
-    uint8_t c = a & b;
-    printf("%d\n",c);
-    if(c){
-        printf("true\n");
-    } else {
-        printf("false\n");
-    }
 }
-
 
 int main()
 {
