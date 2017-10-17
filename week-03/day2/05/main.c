f48a56d2ade4ff40b35893e26f027204f2dc0ff7
batorsimon <bator.simon8@gmail.com>
2017-10-17 11:39:19 +0200

    task 5 done

--- week-03/day2/05/main.c
+++ week-03/day2/05/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+//TODO: write a function which returns 485 as unsigned integer value
+//put your return value in an integer variable in which you should have 229, and print it out
+//try to figure out which type casting you should use
+
+int giving_back_value();
 
 int main()
 {
-    printf("Hello world!\n");
+    int b = 0;
+    b = giving_back_value();
+    printf("%d\n", b);
     return 0;
+}
+
+int giving_back_value()
+{
+    uint32_t a = 485;
+    return a;
+
 }
