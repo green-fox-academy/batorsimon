a85818673030f0d8d7acfadbcedd7ca3cb9d2bc9
batorsimon <bator.simon8@gmail.com>
2017-10-17 10:07:28 +0200

    task 02 done

--- week-03/day2/02/main.c
+++ week-03/day2/02/main.c
@@ -3,6 +3,10 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    double x = 1.2;
+    //TODO: write a program which adds up x and 1, and print out it rounded.
+    printf("%.0f\n", x+1); //rounding
+    //printf("%f\n", x+1); //result = 2,2
+
     return 0;
 }
