072011ca93810b95ad4ea452d3c0097339b6497a
batorsimon <bator.simon8@gmail.com>
2017-10-17 16:29:40 +0200

    task 8 done'
    exit

--- week-03/day2/08/main.c
+++ week-03/day2/08/main.c
@@ -15,26 +15,34 @@ int main()
 
     printf("Give me a number to transform it in an other system: ");
     scanf("%s", str);
-    printf("Give me the original base: ");
-    scanf("%d", &original_base);
+    //printf("Give me the original base: "); not necessary
+    //scanf("%d", &original_base);
     printf("Give me the number of the new base: ");
     scanf("%d", &new_base);
 
     transform(str, original_base, new_base);
-
-
     return 0;
 }
 
 void transform(char str[], int original_base, int new_base)
 {
     char buffer[20];
+    int a = atoi(str);
 
     if(new_base == 2) {
-        itoa(str, buffer, original_base);
-        printf("The original value of %d is %s.\n", , original_base);
-        itoa(, buffer, new_base);
-        printf("The binary value of %d is %s.\n", a, new_base);
+        itoa(a, buffer, new_base);
+        printf("The binary value of %s is %s.\n", str, buffer);
+    } else if(new_base == 8) {
+        itoa(a, buffer, new_base);
+        printf("The octal value of %s is %s.\n", str, buffer);
+    } else if(new_base == 10) {
+        itoa(a, buffer, new_base);
+        printf("The decimal value of %s is %s.\n", str, buffer);
+    } else if(new_base == 16) {
+        itoa(a, buffer, new_base);
+        printf("The hexadecimal value of %s is %s.\n", str, buffer);
+    } else{
+        printf("Try again with a valid base!");
+        exit(0);
     }
-
 }
