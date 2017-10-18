c0a394553a2db0d8eac9b594bd565cc274bbf169
batorsimon <bator.simon8@gmail.com>
2017-10-18 14:59:44 +0200

    task 13 done

--- week-03/day3/13/main.c
+++ week-03/day3/13/main.c
@@ -4,11 +4,36 @@
 int main()
 {
     char str[] = "This is a sample string";
-    char *first, *last;
+  //  char *first, *last;
 
 	//TODO: write a program which prints out the distance between the first and last occurance of character 's'
 
 
+	printf("%s\n", str);
+
+    int first = 0;
+    int last = 0;
+    int i = 0;
+    int distance = 0;
+
+    for(i = 0; i < sizeof str; i++){
+        if(str[i] == 's'){
+            first = i;
+            printf( "The letter 's' is can be found first time at the %d. position.\n", first);
+             break;
+        }
+
+    }
+    for(i = sizeof str; i > 0; i--){
+        if(str[i] == 's'){
+            last = i;
+            printf( "The letter 's' is can be found last time at the %d. position.\n", last);
+             break;
+        }
+
+    }
+    distance = last - first;
+    printf("The distance between the first and the last 's' character is %d.\n", distance);
 
     return 0;
 }
