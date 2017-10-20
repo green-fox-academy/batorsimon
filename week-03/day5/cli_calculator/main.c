cf9fcd08704da1658203451e24c4f79f357eb96a
batorsimon <bator.simon8@gmail.com>
2017-10-20 15:00:59 +0200

    another use of tokens

---
+++ week-03/day5/cli_calculator/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char buf[20];
+    printf("[number] (space) [operator] (space) [number]\n");
+    gets(buf);
+    int i = 0;
+    char *p = strtok (buf, " ");
+    char *array[3];
+
+    //tokenizing a string and putting the elements into an array
+    while (p != NULL)
+    {
+        array[i++] = p;
+        p = strtok (NULL, " ");
+    }
+
+    //with a for loop, print out each element of the array
+    for (i = 0; i < 3; ++i)
+        printf("%s\n", array[i]);
+
+    return 0;
+}
