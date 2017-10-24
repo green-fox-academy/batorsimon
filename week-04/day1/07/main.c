375c0b301b61a69f7106b4fad4403f51d6c3fa13
batorsimon <bator.simon8@gmail.com>
2017-10-24 13:23:35 +0200

    task 7 done

--- week-04/day1/07/main.c
+++ week-04/day1/07/main.c
@@ -3,6 +3,22 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int high_number = 6655;
+    int low_number = 2;
+
+    int* hight_number_pointer = &low_number;
+    int* low_number_pointer = &high_number;
+    //TODO:
+    // Please fix the problem and swap where the pointers point,
+    // without using the "&" operator.
+
+
+    int *a = hight_number_pointer;
+    hight_number_pointer = low_number_pointer;
+    low_number_pointer = a;
+
+    printf("High number pointer: %d.\n", *hight_number_pointer);
+    printf("Low number pointer: %d.\n", *low_number_pointer);
+    printf("a pointer: %d.\n", *a);
     return 0;
 }
