1d38f9af57b1c4ce71201a2ed040bc6160d084fb
batorsimon <bator.simon8@gmail.com>
2017-10-24 13:38:25 +0200

    task 9 done

--- week-04/day1/09/main.c
+++ week-04/day1/09/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+void safe_union(int *a, int l_a, int *b, int l_b, int *result, int l_res) {
+    //TODO:
+    // Implement this function to create the union of a and b into the array result!
+
+    int i = 0;
+    for(i = 0; i < 3; i++){
+        result[i] = a[i];
+        result[i+3] = b[i];
+    }
+
+    return result;
+}
+
+int main(int argc, char** argv) {
+    int a[3] = {1,2,3};
+    int b[3] = {4,5,6};
+    int c[6];
+
+    safe_union(a,3,b,3,c,6);
+
+    int i = 0;
+    for (i = 0; i<6; i++) {
+        printf("%d\n", c[i]);
+    }
+
     return 0;
 }
