e63e01b776a44281c57d5ee2a13f73db0c65f13d
batorsimon <bator.simon8@gmail.com>
2017-10-24 13:13:29 +0200

    task 5,6 are done

--- week-04/day1/05/main.c
+++ week-04/day1/05/main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int numbers[] = {5, 6, 7, 8, 9};
+    int *number_pointer = &numbers[0];
+    //TODO:
+    // The "number_pointer" should point to the third element of the array called "numbers",
+    // than please print its value with it. Solve the problem without reusing the "&" operator
+    // for getting the address of the third element.
+
+    printf("Values of the third element: %d.\n", *(number_pointer+2));
+    printf("The address of the third element: %p.\n", numbers+2);
     return 0;
 }
