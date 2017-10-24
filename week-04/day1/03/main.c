5a92fdc3552f4009eb53ea9b3c59cfdf5d03fc57
batorsimon <bator.simon8@gmail.com>
2017-10-24 13:01:39 +0200

    task 4 is done

--- week-04/day1/03/main.c
+++ week-04/day1/03/main.c
@@ -3,6 +3,12 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int number = 1234;
+    int* number_pointer = &number;
+    //TODO:
+    // update the value of number variable to 42 using the "number_pointer"
+
+    *number_pointer = 42;
+    printf("%d\n", number);
     return 0;
 }
