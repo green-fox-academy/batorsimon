94fc4be263c385ba5cadc5ae37b4d6b361386266
batorsimon <bator.simon8@gmail.com>
2017-10-25 19:50:24 +0200

    task 03 done

---
+++ week-04/day3/03/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct {
+    int x;
+    int y;
+} point_t;
+
+// Todo: Create a function the constructs a point
+// It should take it's x and y coordinate as parameter
+// Create a function that takes 2 Points as a pointer and returns the distance between them
+
+point_t point(int x, int y) {
+    point_t point;
+    point.x = x;
+    point.y = y;
+
+    return point;
+}
+
+float distance(point_t point1, point_t point2) {
+    float a = sqrt(pow((point2.x - point1.x),2) + pow((point2.y - point1.y),2));
+    return a;
+}
+int main()
+{
+    point_t point1 = {4, 8};
+    point_t point2 = {10, 3};
+
+    printf("%f", distance(point1, point2));
+    return 0;
+}
