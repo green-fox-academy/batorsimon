c52be54eaf8c9b58c8323d161904f48132eec4d2
batorsimon <bator.simon8@gmail.com>
2017-10-25 17:05:32 +0200

    task 02 done

--- week-04/day3/02/main.c
+++ week-04/day3/02/main.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// TODO:
+// Write a function that takes a Car as an argument and prints all it's stats
+// If the car is a Tesla it should not print it's gas level
+
 enum CarType {VOLVO, TOYOTA, LAND_ROVER, TESLA};
 
 struct Car {
   enum CarType type;
   double km;
   double gas;
-} carr;
+};
 
-// TODO:
-// Write a function that takes a Car as an argument and prints all it's stats
-// If the car is a Tesla it should not print it's gas level
+void print(struct Car carr) {
+    printf("Type: %d\n", carr.type);
+    printf("km: %.2f\n", carr.km);
+
+    if(carr.type != TESLA) {
+        printf("gas: %f\n", carr.gas);
+    }
+}
 
 int main() {
 
-    struct Car VOLVO = ("Volvo", "100", "12");
-    struct Car TOYOTA = (Toyota,"100", "8");
-    struct Car LAND_ROVER = (Land Rover, "100", "15", );
-    struct Car TESLA = (Tesla, "100", "0");
+    struct Car a1 = {VOLVO, 100, 12};
+    struct Car a2 = {TOYOTA, 100, 8};
+    struct Car a3 = {LAND_ROVER, 100, 15};
+    struct Car a4 = {TESLA, 100, 10};
 
-    printf(" %d\n %d\n", carr.km, carr.gas);
+    print(a1);
+    print(a2);
+    print(a3);
+    print(a4);
 
     return 0;
 }
