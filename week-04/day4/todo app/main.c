556068e8bebdef9169bb02e06a9d782ece6fadf4
batorsimon <bator.simon8@gmail.com>
2017-10-26 17:53:28 +0200

    scanf not working properly have to use gets, pointers,arrays working

--- week-04/day4/todo app/main.c
+++ week-04/day4/todo app/main.c
@@ -3,100 +3,121 @@
 #include <math.h>
 #include <windows.h>
 
+int i = 0;
+int counter = 0;
+
 void menu()
 {
     system("cls");
-    printf("        Todo application\n");
-    printf("========================================\n");
-    printf("Commands:\n");
-    printf(" -a    Adds a new task\n");
-    printf(" -wr   Write current todos to file\n");
-    printf(" -rd   Read todos from a file\n");
-    printf(" -l    Lists all the tasks\n");
-    printf(" -e    Empty the list\n");
-    printf("-rm    Removes a task\n");
-    printf(" -c    Completes a task\n");
-    printf(" -p    Add priority to a task\n");
-    printf(" -lp   Lists all the tasks by priority\n");
-    printf("=========================================\n");
-    printf(" menu  Opens the main menu\n");
-    printf(" clr  Clear command line\n");
-    printf(" exit  Exiting from the program\n");
-    printf("=========================================\n");
+    printf("------------------------------------------\n");
+    printf("|        Todo application                |\n");
+    printf("------------------------------------------\n");
+    printf("|Commands:                               |\n");
+    printf("| -a    Adds a new task                  |\n");
+    printf("| -wr   Write current todos to file      |\n");
+    printf("| -rd   Read todos from a file           |\n");
+    printf("| -l    Lists all the tasks              |\n");
+    printf("| -e    Empty the list                   |\n");
+    printf("| -rm   Removes a task                   |\n");
+    printf("| -c    Completes a task                 |\n");
+    printf("| -p    Add priority to a task           |\n");
+    printf("| -lp   Lists all the tasks by priority  |\n");
+    printf("------------------------------------------\n");
+    printf("| menu  Opens the main menu              |\n");
+    printf("| clr   Clear command line               |\n");
+    printf("| exit  Exiting from the program         |\n");
+    printf("------------------------------------------\n");
     Sleep(5000);
     system("cls");
 }
 
-struct task {
-	char user_input[50];
+typedef struct{
+	char task_name[50];
 	int done;
 	int prio;
-};
+} task;
 
-void input(char* user_input)
-{
-    scanf("%s", user_input);
-    if (strcmp(user_input, "exit") == 0) {
-        printf("The program is exiting now. Goodbye!");
-        exit(0);
-    } else if (strcmp(user_input, "clr") == 0) {
-        system("cls");
-        scanf("%s", user_input);
-    } else if (strcmp(user_input, "menu") == 0) {
-        menu();
-        scanf("%s", user_input);
-    }
-}
-
-void new_task(char* command)
+void new_task(task *todo_arr, char *user_input)
 {
     printf("Adds a new task!\n");
+    printf("%s", user_input);
+    strcpy(todo_arr[i].task_name, user_input);
+    i++;
 
 }
 
-void write_to_file(char* command)
+void write_to_file(task *todo_arr, char *user_input)
 {
+
    printf("Write current todos to file!\n");
+   /*
+   FILE *cd = fopen(command, "w+");
+    if (cd == NULL) {
+        printf("Could not create file: %s", command);
+        return;
+    }
 
+    fprintf(cd, "10\tthis is the multi-word string");
+    fclose(cd);
+   */
 }
 
-void read_from_file(char* command)
+void read_from_file(task *todo_arr)
 {
+    /*
     printf("Read todos from a file!\n");
+    FILE *cd = fopen(command, "r");
+    if (cd == NULL) {
+        printf("The file %s does not exist!", command);
+        return;
+    }
 
+    int num = 0;
+    char str[255];
+
+    fscanf(cd, "%d\t%s", &num, str);
+    printf("read_file_fscanf() - number: %d, string: %s\n", num, str);
+    fclose(cd);
+    */
 }
 
-void list_tasks(char* command)
+void list_tasks(task *todo_arr, char *user_input)
 {
     printf("Lists all the tasks!\n");
 
 }
 
-void empty_task_list(char* command)
+void empty_task_list(task *todo_arr, char *user_input)
 {
     printf("Empty the list!\n");
 
 }
 
-void remove_task(char* command)
+void remove_task(task *todo_arr, char *user_input)
 {
     printf("Removes a task!\n");
 
 }
 
-void completes_task(char* command)
+void completes_task(task *todo_arr, char *user_input)
 {
     printf("Completes a task!\n");
+    printf("Num | Tasks | Prio\n");
+    printf("1 - [ ] Walk the dog\n");
+    printf("2 - [ ] Buy milk\n");
+    printf("3 - [x] Do homework\n");
+    printf("4 - [ ] Do something\n");
+    printf("5 - [ ] Feed the Monkey\n");
 
 }
 
-void priority_to_task(char* command)
+void priority_to_task(task *todo_arr, char *user_input)
 {
     printf("Add priority to a task!\n");
 
 }
 
-void list_tasks_priority(char* command)
+void list_tasks_priority(task *todo_arr, char *user_input)
 {
     printf("Lists all the tasks by priority!\n");
 
@@ -106,43 +127,53 @@ void list_tasks_priority(char* command)
 int main(int argc, char** argv)
 {
     char user_input[80];
-    char command[30];
+    char command[80];
 
     menu();
 
+    task todo_arr[10];
+
     do {
-        input(user_input);
 
-        if (strcmp(user_input, "-a") == 0) {
-            new_task(command);
+        fgets("%s", command);
+        fgets("%s", user_input);
+        if (strcmp(command, "exit") == 0) {
+            printf("The program is exiting now. Goodbye!");
+            exit(0);
+        } else if (strcmp(command, "clr") == 0) {
+            system("cls");
+            continue;
+        } else if (strcmp(command, "menu") == 0) {
+            menu();
+            continue;
+        } else if(strcmp(command, "-a") == 0){
+            new_task(todo_arr, user_input);
+        } else if (strcmp(command, "-wr") == 0) {
+             write_to_file(todo_arr, user_input);
 
-        } else if (strcmp(user_input, "-wr") == 0) {
-             write_to_file(command);
+        } else if (strcmp(command, "-rd") == 0) {
+            read_from_file(todo_arr);
 
-        } else if (strcmp(user_input, "-rd") == 0) {
-            read_from_file(command);
+        } else if (strcmp(command, "-l") == 0) {
+             list_tasks(todo_arr, user_input);
 
-        } else if (strcmp(user_input, "-l") == 0) {
-             list_tasks(command);
+        } else if (strcmp(command, "-e") == 0) {
+             empty_task_list(todo_arr, user_input);
 
-        } else if (strcmp(user_input, "-e") == 0) {
-             empty_task_list(command);
+        } else if (strcmp(command, "-rm") == 0) {
+             remove_task(todo_arr, user_input);
 
-        } else if (strcmp(user_input, "-rm") == 0) {
-             remove_task(command);
+        } else if (strcmp(command, "-c") == 0) {
+            completes_task(todo_arr, user_input);
 
-        } else if (strcmp(user_input, "-c") == 0) {
-            completes_task(command);
+        } else if (strcmp(command, "-p") == 0) {
+            priority_to_task(todo_arr, user_input);
 
-        } else if (strcmp(user_input, "-p") == 0) {
-            priority_to_task(command);
-
-        } else if (strcmp(user_input, "-lp") == 0) {
-            list_tasks_priority(command);
+        } else if (strcmp(command, "-lp") == 0) {
+            list_tasks_priority(todo_arr, user_input);
 
         } else {
             printf("Wrong task name. Look at the possible tasks again.\n");
-            //menu();
         }
 
 
