66cdea020d4ee58b86f68813cbe108c713f6913e
batorsimon <bator.simon8@gmail.com>
2017-10-30 11:19:48 +0100

    started array_04

---
+++ week-05/day1/array_04/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Create 9 arrays of characters, each should contain a word.
+ * Let it be this: The Quick brown Dog jumps over the Lazy Fox.
+ * Create an array of 9 pointers of chractrer type.
+ * Make it so, that each pointer points to one of the words.
+ * Print them out, each in a new line using this array of pointers.
+ * Now make it so, that the Fox is Green and he jumps over the lazy dog.
+ * Use the existing arrays to achive this.
+ * Print it out again.
+ */
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
