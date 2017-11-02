0060224f9f835031a98e32b139b471c5b8f049dd
batorsimon <bator.simon8@gmail.com>
2017-11-02 10:46:29 +0100

    started structure task

---
+++ week-05/day4/structures_functions_00/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Create a structure representing the data of a book:
+ *    * Title
+ *    * Author
+ *    * Year of publication
+ *
+ * Create functions for:
+ *    * Creating a new book.
+ *    * Changeing or setting the Title
+ *    * Changeing or setting the Author
+ *    * Changeing or setting the Year of publication
+ *    * Getting each of these separately by a singel function each.
+ *
+ * IMPORTANT: The first parameter of each function should be a "Book", a variable of the type of this structure.
+ *
+ * Create a main where You demonstrate that each of these works and print out the necessary info.
+ * IMPORTANT: You should only print from the main function. All other functions should return the info needed.
+ */
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
