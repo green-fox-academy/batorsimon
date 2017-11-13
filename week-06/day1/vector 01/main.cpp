5c2f9c130f1b60db925d683c79c039d4ca8de9c6
batorsimon <bator.simon8@gmail.com>
2017-11-13 16:49:55 +0100

    vector 01 done

--- week-06/day1/vector 01/main.cpp
+++ week-06/day1/vector 01/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
 	//create an integer vector with the size of 5 and print all elements of it
 
-  return 0;
+	vector <int> int_vector;
+
+    int_vector.push_back(21);
+    int_vector.push_back(44);
+    int_vector.push_back(5);
+    int_vector.push_back(18);
+    int_vector.push_back(9);
+
+    for(unsigned int i = 0; i < int_vector.size(); ++i) {
+        cout << int_vector[i] << endl;
+    }
+    int_vector.pop_back();
+
+    return 0;
 }
