56eb6d4df19dab82d0a213ed93f7dcfd44e2594f
batorsimon <bator.simon8@gmail.com>
2017-11-13 14:06:47 +0100

    task string 02 done

--- week-06/day1/string 02/main.cpp
+++ week-06/day1/string 02/main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+// Define several things as a variable then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a double
+// Whether you are married or not as a boolean
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    string name = "Simon Bator";
+    int age = 24;
+    double height = 1.8;
+    bool married = 0;
+
+    cout << "My name: " << name << endl;
+    cout << "My age: " << age << endl;
+    cout << "My height in meter: " << height << endl;
+    cout << "Do I married? " << married << endl;
+
     return 0;
 }
