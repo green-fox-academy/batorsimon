84491dae81e6397986689610aa32d3f4ae0a4c44
batorsimon <bator.simon8@gmail.com>
2017-11-13 15:35:17 +0100

    task namespace 04 done

--- week-06/day1/namespace 04/main.cpp
+++ week-06/day1/namespace 04/main.cpp
@@ -13,6 +13,7 @@
 // create a function that can add the two age, and returns it
 
 using namespace std;
+
 namespace Mark {
     string name = "Simon Bator";
     int age = 24;
@@ -27,12 +28,12 @@ namespace Mark {
 
 namespace Maria {
     string name = "Simon Bator";
-    int age = 24;
+    int age2 = 24;
     int favorite_number = 7;
 
     void func() {
         cout << "My name: " << name << endl;
-        cout << "My age: " << age << endl;
+        cout << "My age: " << age2 << endl;
         cout << "My favorite number: " << favorite_number << endl;
     }
 }
@@ -40,15 +41,15 @@ namespace Maria {
 using namespace Mark;
 using namespace Maria;
 
-int returning(int age, int age)
+int returning(int age, int age2)
 {
-    int add = age + age;
+    int add = age + age2;
     return add;
 }
 
 int main()
 {
-    int add_main = returning(age, age);
+    int add_main = returning(age, age2);
     cout << "The years added together: " << add_main<<endl;
 
     return 0;
