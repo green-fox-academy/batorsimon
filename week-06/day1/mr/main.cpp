5b4c45e6d4867e76d30e2d9c3549a8e24cd7dd9f
batorsimon <bator.simon8@gmail.com>
2017-11-13 10:47:46 +0100

    code rw

--- week-06/day1/mr/main.cpp
+++ week-06/day1/mr/main.cpp
@@ -5,6 +5,18 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+
+    bool married = true;
+    string name = "Levente";
+
+    cout << "My name is: \n" << name << endl;
+
+    if(married) {
+        cout << "Not this time" << endl;
+    } else {
+        cout << "NEED coffee" << endl;
+    }
+
     return 0;
 }
 
