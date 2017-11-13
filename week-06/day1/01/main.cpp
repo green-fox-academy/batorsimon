ed58687bcd9bc8aa8209a6b3e847fc257b6a007d
batorsimon <bator.simon8@gmail.com>
2017-11-13 13:29:07 +0100

    task 01 done

---
+++ week-06/day1/01/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+// Store their names in separate string type variables
+
+int main()
+{
+    string name1 = "Bazsi";
+    string name2 = "Bence";
+    string name3 = "Gergo";
+
+    cout << "Hello, " << name1 << "!" << endl;
+    cout << "Hello, " << name2 << "!" << endl;
+    cout << "Hello, " << name3 << "!" << endl;
+
+    return 0;
+}
