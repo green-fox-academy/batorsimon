306caaeaea949cdf3df8eef62f71a77724c95831
batorsimon <bator.simon8@gmail.com>
2017-11-13 22:47:37 +0100

    vector tasks done

--- week-06/day1/vector 03/main.cpp
+++ week-06/day1/vector 03/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <random>
+#include <ctime>
 
 using namespace std;
 
@@ -7,7 +10,25 @@ int main() {
 	//create a vector of integers with the size of 20
 	//fill this vector with random numbers from 0 to 10
 	//print the items of the vector
-  //remove the even numbers, then print the items again
+    //remove the even numbers, then print the items again
 
-  return 0;
+    vector<int> vectors(20);
+    srand(time(0));
+
+    for (int i = 0; i < vectors.size(); i++) {
+        vectors[i] = rand() % 11;
+    }
+
+    for (int i = 0; i < vectors.size(); i++) {
+        cout << vectors[i] << " ";
+    }
+
+    cout << endl << "----------" << endl;
+
+    for (int i = 0; i < vectors.size(); i++) {
+        if(vectors[i] % 2 == 0){
+            cout << vectors[i] << " ";
+        }
+    }
+    return 0;
 }
--- week-06/day1/vrctor 02/main.cpp
+++ week-06/day1/vrctor 02/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <random>
+#include <ctime>
 
 using namespace std;
 
@@ -7,5 +10,16 @@ int main() {
 	//Create a vector of integers
 	//reserve place for 10 element, then fill those place with random numbers between 0 and 10
 
-  return 0;
+    vector<int> vectors(10);
+    srand(time(0));
+
+    for(int i = 0; i < 10; i++) {
+        vectors[i] = rand() % 11;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << vectors[i] << " ";
+    }
+
+    return 0;
 }
