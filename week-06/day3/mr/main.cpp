aab27e2c0c4772794d73674f0ddecde767e3854c
batorsimon <bator.simon8@gmail.com>
2017-11-15 10:10:03 +0100

    started material review

---
+++ week-06/day3/mr/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class vehicle{
+    public:
+        void move() {
+            cout << " Vehicle is moving!" << endl;
+        }
+
+    protected:
+        int protectedint;
+
+    private:
+        int privateint;
+
+};
+
+class ElectricVehicle: public vehicle{
+    public:
+        int consumtion;
+
+};
+
+class EAuto: public ElectricVehicle{
+    public:
+        int range;
+};
+
+class Tesla: public ElectricVehicle{
+public:
+    void autopilot();
+};
+int main()
+{
+    vehicle v;
+    v.move();
+
+    EAuto ea;
+    ea.consumtion = 12;
+    ea.range = 200;
+
+    cout << "EA properities" << endl;
+    ea.move();
+    cout << " - has consumation" << ea.consumtion << endl;
+    cout << " - has range" << ea.range << endl;
+
+
+
+    return 0;
+}
