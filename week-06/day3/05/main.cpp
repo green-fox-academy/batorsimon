fce339a96dbff116e88aaeea3ed4f6bd60635ef9
batorsimon <bator.simon8@gmail.com>
2017-11-15 17:22:14 +0100

    task 05 done without assignening seats

--- week-06/day3/05/main.cpp
+++ week-06/day3/05/main.cpp
@@ -11,49 +11,74 @@ using namespace std;
 // Inherite at least two car models and try out the methods you have created.
 
 class Car{
-public:
-    void set_age(int age){
-        this->age = age;
-    };
-    void set_km_runed(int km_runed){
-        this->km_runed = km_runed;
-    };
-    void set_manufacturer(string manufacturer){
-        this->manufacturer = manufacturer;
-    };
-    void set_number_of_seats(int number_of_seats){
-        this->number_of_seats = number_of_seats;
-    };
+    public:
+        void set_age(int age){
+            this->age = age;
+        };
+        void set_km_runed(int km_runed){
+            this->km_runed = km_runed;
+        };
+        void set_manufacturer(string manufacturer){
+            this->manufacturer = manufacturer;
+        };
+        void set_number_of_seats(int number_of_seats){
+            this->number_of_seats = number_of_seats;
+        };
 
-    void get_age(){
-        cout << "The age of the car is: " << age <<endl;
-    };
-    void get_km_runed(){
-        cout << "The age of the car is: " << km_runed <<endl;
+        void get_age(){
+            cout << "The age of the car is: " << age <<endl;
+        };
+        void get_km_runed(){
+            cout << "The km-s the car have run: " << km_runed <<endl;
+            }
+        void get_manufacturer(){
+            cout << "The manufacturer of the car is: " << manufacturer <<endl;
         }
-    void get_manufacturer(){
-        cout << "The age of the car is: " << manufacturer <<endl;
-    }
-    void get_number_of_seats(){
-        cout << "The age of the car is: " << number_of_seats <<endl;
-    }
+        void get_number_of_seats(){
+            cout << "The number of seats in the car is: " << number_of_seats <<endl;
+        }
 
-private:
-    int age;
-    int km_runed;
-    string manufacturer;
-    int number_of_seats;
+    private:
+        int age;
+        int km_runed;
+        string manufacturer;
+        int number_of_seats;
 };
+
+class Audi: public Car{
+
+};
+
+class Peugeut: public Car{
+
+};
+
 
 int main()
 {
-    Car c;
-    c.set_age(11);
-    /*
-    c.age = 11;
-    c.km_runed = 192500;
-    c.manufacturer = "Peugeut";
-    c.number_of_seats = 5;
-    */
+    cout << "Peugeut: " << endl;
+
+    Peugeut p;
+    p.set_age(11);
+    p.get_age();
+    p.set_km_runed(192500);
+    p.get_km_runed();
+    p.set_manufacturer("Peugeut");
+    p.get_manufacturer();
+    p.set_number_of_seats(5);
+    p.get_number_of_seats();
+
+    cout << endl << "Audi: " << endl;
+
+    Audi a;
+    a.set_age(6);
+    a.get_age();
+    a.set_km_runed(2500);
+    a.get_km_runed();
+    a.set_manufacturer("Audi");
+    a.get_manufacturer();
+    a.set_number_of_seats(4);
+    a.get_number_of_seats();
+
     return 0;
 }
