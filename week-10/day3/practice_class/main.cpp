6febe9516f7950d6ff6b5a4725720cf37438048e
batorsimon <bator.simon8@gmail.com>
2017-12-13 13:42:01 +0100

    practise vectors and classes

---
+++ week-10/day3/practice_class/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class student{
+public:
+    student() {}
+    student(int grade){
+        this->grade=grade;
+    }
+
+    void add_grade(int grade){
+        this->grade = grade;
+    }
+
+    int get_grade(){
+        return grade;
+    }
+
+    void add_grade_vector(int grade){
+        grades.push_back(grade);
+    }
+
+    vector<int> grades;
+
+private:
+    int grade;
+
+};
+
+int main() {
+    // Create a student Class
+    // that has a method `add_grade`, that takes a grade from 1 to 5
+    // an other method `get_average`, that returns the average of the
+    // grades
+
+    student st;
+   // st.add_grade(4);
+
+    st.add_grade_vector(5);
+    st.add_grade_vector(4);
+    st.add_grade_vector(3);
+    st.add_grade_vector(2);
+
+    for(int i = 0; i < st.grades.size(); i++){
+        cout << st.grades.at(i) << endl;
+    }
+
+
+  return 0;
+
+}
