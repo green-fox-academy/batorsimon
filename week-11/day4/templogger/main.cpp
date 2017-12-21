cffa5ac39a454c90be0338aa7f8e3c03731b9449
batorsimon <bator.simon8@gmail.com>
2017-12-21 15:57:37 +0100

    done with logging function

--- week-11/day4/templogger/main.cpp
+++ week-11/day4/templogger/main.cpp
@@ -11,6 +11,8 @@ A 3. Parameter a file neve amibe berakja  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -63,39 +65,83 @@ public:
 };
 
 void user_input();
-void logger();
+void logger(string x, int a, string y);
 
 int main()
 {
     srand(time(NULL));
-
-    TempGen tn;
-    Celsius cs;
-    Fahrenheit ft;
-    Kelvin kn;
-/*
-    tn.set_temp(rand() % 20 - 10);
-    cout <<  tn.get_temp() << endl;
-
-    cs.set_temp(tn.get_temp());
-    cout <<  cs.get_temp() << endl;
-
-    ft.set_temp(tn.get_temp());
-    cout <<  ft.get_temp() << endl;
-
-    kn.set_temp(tn.get_temp());
-    cout <<  kn.get_temp() << endl;
-*/
     user_input();
-    logger();
 
     return 0;
 }
 
 void user_input(){
 
+    string user_input_date;
+    int user_input_days;
+    string user_input_file_name;
+
+    cout << "Give in 3 parameters: " << endl << "Firstly the date without separation." << endl;
+    cout << "Secondly how many days you want to get the temperatures." << endl;
+    cout << "Thirdly the name of the file you want to write the temperatures. \n" << endl;
+
+    cin >> user_input_date;
+    if(!cin){
+        cout << "Please give in a valid date." << endl;
+         cin.clear();
+    }
+
+    cin >> user_input_days;
+    if(!cin){
+        cout << "Please give in a valid number." << endl;
+         cin.clear();
+    }
+
+
+    cin >> user_input_file_name;
+     if(!cin){
+        cout << "Please give in a valid file name." << endl;
+         cin.clear();
+    }
+
+    logger(user_input_date, user_input_days, user_input_file_name);
+
 }
 
-void logger(){
+void logger(string x, int a, string y){
+
+    TempGen tn;
+    Celsius cs;
+    Fahrenheit ft;
+    Kelvin kn;
+
+    time_t now = time(0);
+    char* dt = ctime(&now);
+
+    string file_name = y + ".txt";
+    ifstream infile;
+    ofstream outfile;
+    outfile.open(file_name.c_str());
+
+    outfile << "The given date: " << x <<endl;
+    outfile << "The given number of days: " << a <<endl;
+    outfile << "The request were made at: " << endl << dt << endl;
+    outfile << "-----------------------------" << endl;
+
+    for(unsigned int i = 0; i < (a*24); i++){
+        tn.set_temp(rand() % 20 - 10);
+        outfile << "The temperature in Fahrenheit: " << tn.get_temp() << endl;
+
+        cs.set_temp(tn.get_temp());
+        outfile << "The temperature in Celsius: " << cs.get_temp() << endl;
+
+        ft.set_temp(tn.get_temp());
+        outfile << "The temperature in Fahrenheit: " << ft.get_temp() << endl;
+
+        kn.set_temp(tn.get_temp());
+        outfile << "The temperature in Kelvin: " << kn.get_temp() << endl;
+    }
+
+    outfile.close();
 
 }
