4d7d5fb0f04c84aa662efc7398e69155222e273a
batorsimon <bator.simon8@gmail.com>
2017-12-21 14:32:46 +0100

    3 inherit classes with working methods

--- week-11/day4/templogger/main.cpp
+++ week-11/day4/templogger/main.cpp
@@ -15,40 +15,87 @@ A 3. Parameter a file neve amibe berakja  */
 using namespace std;
 
 class TempGen{
-    private:
-        int temp;
+    protected:
+        float temp;
 
     public:
-        void set_temp(int temp){
+        void set_temp(float temp){
             this->temp = temp;
         }
 
-        int get_temp(){
+        float get_temp(){
             return temp;
         }
 
 };
 
-class Celsius{
+class Celsius: public TempGen{
+public:
+      void set_temp(float temp){
+            this->temp = temp;
+        }
 
+        float get_temp(){
+            return temp;
+        }
 };
 
-class Fahrenheit{
+class Kelvin: public TempGen{
+public:
+     void set_temp(float temp){
+            this->temp = temp + 273.15;
+        }
 
+        float get_temp(){
+            return temp;
+        }
 };
 
-class Kelvin{
+class Fahrenheit: public TempGen{
+public:
+     void set_temp(float temp){
+            this->temp = temp * 1.8000 + 32.00;
+        }
 
+        float get_temp(){
+            return temp;
+        }
 };
+
+void user_input();
+void logger();
 
 int main()
 {
     srand(time(NULL));
 
     TempGen tn;
+    Celsius cs;
+    Fahrenheit ft;
+    Kelvin kn;
+/*
     tn.set_temp(rand() % 20 - 10);
     cout <<  tn.get_temp() << endl;
 
+    cs.set_temp(tn.get_temp());
+    cout <<  cs.get_temp() << endl;
+
+    ft.set_temp(tn.get_temp());
+    cout <<  ft.get_temp() << endl;
+
+    kn.set_temp(tn.get_temp());
+    cout <<  kn.get_temp() << endl;
+*/
+    user_input();
+    logger();
 
     return 0;
+}
+
+void user_input(){
+
+}
+
+void logger(){
+
 }
