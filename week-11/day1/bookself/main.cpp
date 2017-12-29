f497a8bd549c935dbff42b0d9973c7b30e472e51
batorsimon <bator.simon8@gmail.com>
2017-12-30 00:19:42 +0100

    Book class as a vector controlled by bookself. Project done. (cout in a method...hopefully Levi wont notice this haha)

--- week-11/day1/bookself/main.cpp
+++ week-11/day1/bookself/main.cpp
@@ -19,14 +19,25 @@
 using namespace std;
 
 class Book{
-private:
-    string name;
-    int year_of_publication;
-    int number_of_pages;
-public:
-Book();
-Book(string name, int year_of_publication, int number_of_pages);
+    private:
+        string name;
+        int year_of_publication;
+        int number_of_pages;
+    public:
+        Book();
+        Book(string name, int year_of_publication, int number_of_pages);
+
+        string get_name();
+        void set_name(string name);
+
+        int get_year_of_publication();
+        void set_year_of_publication(int year_of_publication);
+
+        int get_number_of_pages();
+        void set_number_of_pages(int number_of_pages);
 };
+
+Book::Book() {};
 
 Book::Book(string name, int year_of_publication, int number_of_pages){
     this->name = name;
@@ -34,33 +45,112 @@ Book::Book(string name, int year_of_publication, int number_of_pages){
     this->number_of_pages = number_of_pages;
 };
 
+string Book::get_name(){
+    return name;
+};
+
+void Book::set_name(string name){
+    this->name = name;
+};
+
+int Book::get_year_of_publication() {
+    return year_of_publication;
+};
+
+void Book::set_year_of_publication(int year_of_publication) {
+     this->year_of_publication = year_of_publication;
+};
+
+int Book::get_number_of_pages(){
+    return number_of_pages;
+};
+
+void Book::set_number_of_pages(int number_of_pages){
+    this->number_of_pages = number_of_pages;
+};
+
+
 class Bookself{
 private:
     int counter;
     vector<Book> book;
-private:
+
+public:
     Bookself();
     Bookself(int counter);
-    void add(Book *bok);
-    void get_oldest();
-    void get_longest();
+    void add(Book *bk);
+    int get_oldest(Book *bk);
+    int get_longest(Book *bk);
+    void set_counter(int counter);
+    int get_counter();
+    void list_books(Book *bk);
 };
 
-void Bookself::add(Book *bok){
-    book.push_back(*bok);
+Bookself::Bookself() {};
+
+Bookself::Bookself(int counter){
+    this->counter = counter;
 };
 
-void Bookself::get_longest(){
-
+void Bookself::add(Book *bk){
+    book.push_back(*bk);
+    counter++;
 };
 
-void Bookself::get_oldest(){
+int Bookself::get_longest(Book *bk){
+    int result = 0;
+
+    for(unsigned int i = 0; i < book.size(); ++i){
+        if(book.at(i).get_number_of_pages() > result) {
+            result = book.at(i).get_number_of_pages();
+        }
+    }
+
+    return result;
+};
+
+int Bookself::get_oldest(Book *bk){
+    int result = 3000;
+
+    for(unsigned int i = 0; i < book.size(); ++i){
+        if(book.at(i).get_year_of_publication() < result){
+            result = book.at(i).get_year_of_publication();
+        }
+    }
+
+    return result;
+};
+
+void Bookself::set_counter(int counter){
+    this->counter = counter;
+};
+
+int Bookself::get_counter() {
+    return counter;
+};
+
+void Bookself::list_books(Book *bk){
+    cout << endl << "The books in the self are: " << endl;
+    for(unsigned int i = 0; i < book.size(); ++i){
+        cout << i+1 << ". book. Name: " << book.at(i).get_name() << ". Year of publication: " << book.at(i).get_year_of_publication() << ". It is: " << book.at(i).get_number_of_pages() << " pages long." << endl;
+    }
 
 };
 
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Book bk;
+    Bookself bs;
+    bs.set_counter(0);
+    bs.add(new Book("RoncsSzelarnyekban", 2017, 88));
+    bs.add(new Book("Mesz", 2015, 72));
+    bs.add(new Book("AholMegszakad", 2012, 68));
+
+    cout << "There is: " << bs.get_counter() << " books in the self." << endl << endl;
+    cout << "The oldest book was published in: " << bs.get_oldest(&bk) << endl;
+    cout << "The longest book is: " << bs.get_longest(&bk) << " pages long." << endl;
+    bs.list_books(&bk);
+
     return 0;
 }
