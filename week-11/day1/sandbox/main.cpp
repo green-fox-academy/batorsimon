b13ab6ff0ac5b77c2765234bdab675233f60dd4f
batorsimon <bator.simon8@gmail.com>
2017-12-31 17:52:44 +0100

    solution tried out in sandbox project

--- week-11/day1/sandbox/main.cpp
+++ week-11/day1/sandbox/main.cpp
@@ -4,21 +4,63 @@
 
 using namespace std;
 
+typedef struct
+{
+    string a;
+    string b;
+    string c;
+}Mystruct;
+
+Mystruct myfun();
+
 int main()
 {
     /*
     string line;
     line = "1" + "abc" + "2" + "asd";
     cout << line << endl;
+
+    not working !
     */
 
+    /*
     auto v = vector<int>{1, 2, 3};
     std::copy(begin(v), end(v), std::ostream_iterator<int>(std::cout, " "));
 
+    not working!!
+    */
+
+    /*
     vector<string> v1{"abc", "sad", "ASD"};
     vector<int> v2{1, 2, 3};
 
     cout << v1 << endl;
     cout << v2;
+
+    not working !!
+    */
+
+    Mystruct ms2;
+    ms2 = myfun();
+    cout << "Name: " << ms2.a << " Year: " << ms2.b << " Length: " << ms2.c << endl;
+
     return 0;
+}
+
+
+Mystruct myfun()
+{
+    string a;
+    string b;
+    string c;
+    Mystruct ms;
+
+    a = "name";
+    b = "year";
+    c = "length";
+    ms.a = a;
+    ms.b = b;
+    ms.c = c;
+
+    return(ms);
 }
