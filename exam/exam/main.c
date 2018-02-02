095fe7110c6edce97de8ac096695af95a70825b5
batorsimon <bator.simon8@gmail.com>
2018-02-02 10:13:53 +0100

    exam done

--- exam/exam/main.c
+++ exam/exam/main.c
@@ -6,8 +6,9 @@ void multiple(float temp[]);
 
 int main()
 {
-    float array[30] = { 23.4, 55.2, 22.6, 3.83, 40.5, 18.66};
     task();
+
+    float array[30] = { 23.4, 55.2, 22.6, 3.83, 40.5, 18.66};
     multiple(array);
 
     return 0;
@@ -21,9 +22,9 @@ void multiple(float temp[]) {
 
     printf("The array before multiplying: \n");
 
-    for(i; temp[i] != '\0'; i++) {
+    for(i = 0; temp[i] != '\0'; i++) {
         sum += temp[i];
-        printf("%.2f\n", temp[i]);
+        printf("The %d. element: %.2f.\n", i+1, temp[i]);
     }
 
     avg = sum / i;
@@ -33,14 +34,27 @@ void multiple(float temp[]) {
 
      for(i = 0; temp[i] != '\0'; i++) {
         temp[i] = (temp[i] * avg);
-        printf("%.2f \n", temp[i]);
+        sum += temp[i];
+        printf("The %d. element: %.2f.\n", i+1,temp[i]);
     }
+
+    avg = sum / i;
+    printf("\nThe sum of the multiplied array: %f\n", sum);
+    printf("The average of the multiplied array: %f\n\n", avg);
 
 }
 
 void task() {
+
+    printf("1111  1   1     1      11   11\n");
+    printf("1      1 1     1 1     1 1 1 1\n");
+    printf("1111    1   111111111  1  1  1\n");
+    printf("1      1 1   1     1   1     1\n");
+    printf("1111  1   1 1       1  1     1\n\n");
+
     printf("Welcome!\n");
     printf("Today i am going to write a little example of passing an entire array. \n");
     printf("I am going to create a function that takes an array of floats.\n");
-    printf("Then the function multiplies each element with the average of the array.\n\n");
+    printf("Then the function multiplies each element with the average of the array.");
+
 }
